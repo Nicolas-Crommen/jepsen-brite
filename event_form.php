e733d5362dff94e303e8fad0fb9e448c87a91c4c
Nicolas-Crommen <nicolas.crommen@icloud.com>
2020-07-23 11:34:49 +0200

    style form event

--- event_form.php
+++ event_form.php
@@ -1,5 +1,31 @@
 <?php
 include "./includes/templates/header.php";
+?>
+
+<div class="container login-page">
+	<h1 class="text-center">Create event</h1>
+	<form method="post" action="event_form.php">
+		<p>
+			<input class="form-control" type="text" name="title" autocomplete="off" placeholder="Give your event a title" />
+		</p>
+		<p>
+			<textarea class="form-control" name="description" rows="8" cols="45" autocomplete="off" placeholder="Explain what will take place"></textarea>
+		</p>
+		<p>In which category would you like your event to be referenced ?
+			<select class="form-control" name="category">
+				<option value="concert">Concert</option>
+				<option value="expo">Exhibition</option>
+				<option value="cinema">Cinema</option>
+				<option value="talk">Talk</option>
+				<option value="party">Party</option>
+				<option value="other">Other</option>
+			</select>
+		</p>
+		<input class="btn btn-info btn-block" type="submit" name="submit">
+	</form>
+</div>
+
+<?php
 		
 try {
 $bdd = new PDO('mysql:host=localhost;dbname=jepsen-brite-db;charset=utf8','root','root');
@@ -8,44 +34,21 @@ die('Erreur : ' . $e -> getMessage() );
 }
 	
 if (isset($_POST['title']) AND isset($_POST['description']) AND isset($_POST['category'])) {
-	echo $_POST['title'] . $_POST['description'] . $_POST['category'];
 
+	$req = $bdd -> prepare('INSERT INTO events(title,author,description,category,image) VALUES(?, ?, ?, ?, ?)');
 
-	$req = $bdd -> prepare('INSERT INTO events(title,author,description,category) VALUES(?, ?, ?, ?)');
-	echo 'pass';
+	$req -> execute(array($_POST['title'], 1,$_POST['description'], $_POST['category'], 'image.com'));
 
-	$req -> execute(array($_POST['title'], 1,$_POST['description'], $_POST['category']));
-
-	echo $req -> rowCount();
-	
-	echo "Your event has been added to our database";
+	echo '<p align="center" color="green">Your event has been added to our database</p>';
 }
 else
 {
-	echo "All fields are required";
+	echo '<p align="center" color="red">All fields are required !<p>';
 }
  
 ?>
 
-<form method="post" action="event_form.php">
-	<p>
-		<input type="text" name="title" value="Give your event a title" />
-	</p>
-	<p>
-		<textarea name="description" rows="8" cols="45">Explain what will take place</textarea>
-	</p>
-	<p>In which category would you like your event to be referenced ?
-		<select name="category">
-			<option value="concert">Concert</option>
-			<option value="expo">Exhibition</option>
-			<option value="cinema">Cinema</option>
-			<option value="talk">Talk</option>
-			<option value="party">Party</option>
-			<option value="other">Other</option>
-		</select>
-	</p>
-	<input type="submit" name="submit">
-</form>
+
 
 <?php
 include "./includes/templates/footer.php";
