2644eac880b1d5739cc5b9d68deb81026fb93f72
Nicolas-Crommen <nicolas.crommen@icloud.com>
2020-07-28 17:11:34 +0200

    mardi 28 endofday

--- event_form.php
+++ event_form.php
@@ -1,55 +1,54 @@
 <?php
 include "./includes/templates/header.php";
+include "connect.php";
+include "./includes/func/functions.php";
 ?>
 
-<div class="container login-page">
-	<h1 class="text-center">Create event</h1>
-	<form method="post" action="event_form.php">
-		<p>
-			<input class="form-control" type="text" name="title" autocomplete="off" placeholder="Give your event a title" required="required" />
-		</p>
-		<p>
-			<textarea class="form-control" name="description" rows="8" cols="45" autocomplete="off" placeholder="Explain what will take place" required="required"></textarea>
-		</p>
-		<p>In which category would you like your event to be referenced ?
-			<select class="form-control" name="category">
-				<option value="concert">Concert</option>
-				<option value="expo">Exhibition</option>
-				<option value="cinema">Cinema</option>
-				<option value="talk">Talk</option>
-				<option value="party">Party</option>
-				<option value="other">Other</option>
-			</select>
-		</p>
-		<input class="btn btn-info btn-block" type="submit" name="submit">
-	</form>
-</div>
-
 <?php
+if (isset($_SESSION['userid'])) { ?>
+
+	<div class="container login-page">
+		<h1 class="text-center">Create event</h1>
+		<form method="post" action="event_form.php">
+			<p>
+				<input class="form-control" type="text" name="title" autocomplete="off" placeholder="Give your event a title" required="required" />
+			</p>
+			<p><input class="form-control" type="date" name="time" placeholder="yyyy-mm-jj"></p>
+			<p>
+				<textarea class="form-control" name="description" rows="8" cols="45" autocomplete="off" placeholder="Explain what will take place" required="required"></textarea>
+			</p>
+			<p>In which category would you like your event to be referenced ?
+				<select class="form-control" name="category">
+					<option value="" selected disabled hidden>Choose here</option>
+					<option value="concert">Concert</option>
+					<option value="expo">Exhibition</option>
+					<option value="cinema">Cinema</option>
+					<option value="talk">Talk</option>
+					<option value="party">Party</option>
+					<option value="other">Other</option>
+				</select>
+			</p>
+			<input class="btn btn-info btn-block" type="submit" name="submit">
+		</form>
+	</div>
+
+	<?php
 		
-try {
-$bdd = new PDO('mysql:host=localhost;dbname=jepsen-brite-db;charset=utf8','root','root');
-} catch (Exception $e) {
-die('Erreur : ' . $e -> getMessage() );
-}
-	
-if (isset($_POST['title']) AND isset($_POST['description']) AND isset($_POST['category'])) {
+	if (isset($_POST['title']) AND isset($_POST['description']) AND isset($_POST['category']) AND isset($_POST['time'])) {
 
-	$req = $bdd -> prepare('INSERT INTO events(title,author,description,category,image) VALUES(?, ?, ?, ?, ?)');
+		$req = $con -> prepare('INSERT INTO events(title,author,description,category,image,`time`) VALUES(?, ?, ?, ?, ?, ?)');
+		$req -> execute(array($_POST['title'], $_SESSION['userid'],$_POST['description'], $_POST['category'], 'image.com', $_POST['time']));
 
-	$req -> execute(array($_POST['title'], $_SESSION['userid'],$_POST['description'], $_POST['category'], 'image.com'));
+		echo '<p class="" align="center" color="green">Your event has been added to our database</p>';
+	}
+	else
+	{
+		echo '<p align="center" color="red">All fields are required !<p>';
+	}
+} 
 
-	echo '<p class="" align="center" color="green">Your event has been added to our database</p>';
-}
 else
-{
-	echo '<p align="center" color="red">All fields are required !<p>';
-}
- 
-?>
+	{echo 'You must be logged in to access this page. Please <a href="login.php">log to your account</a>';}
 
-
-
-<?php
 include "./includes/templates/footer.php";
 ?>
--- user_events.php
+++ user_events.php
@@ -8,27 +8,77 @@ include "./includes/func/functions.php";
 <?php
 if (isset($_SESSION['userid'])) {
 
-	if (isset ($_GET['do']) && $_GET['do']=='delete')
+// Application des modifs
+	if (isset($_POST['title']) AND isset($_POST['description']) AND isset($_POST['category']) AND isset($_POST['time'])) {
+
+	$edt = $con -> prepare('UPDATE events SET title=?, description=?, category=?, `time`=? WHERE id=?)');
+	$edt -> execute(array($_POST['title'], $_POST['description'], $_POST['category'], $_POST['time'], $_GET['id']));
+
+	echo '<p class="" align="center" color="green">Your event has been modified</p>';
+	}
+	else
 	{
-	$con -> prepare('DELETE FROM events WHERE id=?');
-    $del = $con -> execute(array($_GET['id']))
+		echo '<p align="center" color="red">All fields are required !<p>';
+	}
+		
+
+	
+// Delete event
+	if (isset ($_GET['do']) && isset ($_GET['id']) && $_GET['do']=='delete')
+	{
+	$del = $con -> prepare('DELETE FROM events WHERE id=?');
+    $del -> execute(array($_GET['id']));
     echo "Your event has succesfully been deleted";
 	}
 
-	elseif (isset ($_GET['do']) && $_GET['do']=='edit')
+// Edit event	
+	elseif (isset ($_GET['do']) && isset ($_GET['id']) && $_GET['do']=='edit')
 	{
-	echo 'test edit';
+	$evt = $con -> prepare('SELECT * FROM events WHERE id=?');
+	$evt->execute(array($_GET['id']));
+	$data_edit=$evt->fetchAll();
+?>	
+
+	<div class="events-grid">
+	<?php foreach ($data_edit as $item_edit) { ?>
+	<form method="post" action="<?php echo 'user_events.php?id=' . $_GET['id'] ?>">
+		<p>
+			<input class="form-control" type="text" name="title" autocomplete="off" value="<?php echo $item_edit['title']?>" required="required" />
+		</p>
+		<p><input class="form-control" type="date" name="time" value="<?php echo $item_edit['time']?>"></p>
+		<p>
+			<textarea class="form-control" name="description" rows="8" cols="45" autocomplete="off" required="required"><?php echo $item_edit['description']?></textarea>
+		</p>
+		<p>In which category would you like your event to be referenced ?
+			<select class="form-control" name="category">
+				<option value="" selected disabled hidden>Choose here</option>
+				<option value="concert">Concert</option>
+				<option value="expo">Exhibition</option>
+				<option value="cinema">Cinema</option>
+				<option value="talk">Talk</option>
+				<option value="party">Party</option>
+				<option value="other">Other</option>
+			</select>
+		</p>
+		<input class="btn btn-info btn-block" type="submit" name="submit">
+	</form>
+
+<?php	
+	}
 	}
 
+
+// Display user events	
 	else 
 	{
-	echo '<p class="text-center">Welcome on the home Page ! Take a look at our latest events.</p>';
+	echo '<p class="text-center">Welcome on your events page ! Here you can manage everything about your Jepsen Brite events</p>';
 
-	$lst = $con -> prepare('SELECT * FROM events WHERE author=? ORDER BY "date" DESC');
+	$lst = $con -> prepare('SELECT * FROM events WHERE author=? ORDER BY `time` DESC');
 
 	$lst->execute(array($_SESSION["userid"]));
 	$donnees=$lst->fetchAll();
-	?>
+
+?>
 
 	<div class="events-grid">
 	<?php foreach ($donnees as $item) { ?>
@@ -37,20 +87,17 @@ if (isset($_SESSION['userid'])) {
 			<h5 class="text-center"> <?php echo $item['time']?></h5>
 			<h6 class="text-center"> <?php echo $item['category']?></h6>
 			<p class="text-center">Decription : <?php echo $item['description']?></p>
-			<a href=<?php echo 'user_events.php?do=edit&id=' . $item['id']?> class="btn btn-success text-center btn-block" >Edit</a>
-			<a href=<?php echo 'user_events.php?do=delete&id=' . $item['id']?> class="btn btn-danger text-center btn-block" >Delete</a>
+			<?php echo '<a href="user_events.php?do=edit&id=' . $item['id'] . '" class="btn btn-success text-center btn-block">Edit</a>';?>
+			<?php echo '<a href="user_events.php?do=delete&id=' . $item['id'] . '" class="btn btn-danger text-center btn-block" >Delete</a>';?>
 		</div>
-
-	<?php
-	}
-
-else {
-	echo 'Your are not logged in. Please go to login section in order to access your existing events';
-}	
-}
-?>
-</div>
-
+	<?php } ?>
+	</div>
 <?php
+	}
+}
+
+else
+	{echo 'You must be logged in to access this page. Please <a href="login.php">log to your account</a>';}
+
 include "./includes/templates/footer.php";
 ?>
