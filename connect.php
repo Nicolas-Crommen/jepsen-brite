eafb757292c852d57a0b7fafe19ea7cff0696300
Nicolas Crommen <nicolas.crommen@icloud.com>
2020-07-31 15:21:36 +0200

    images herok

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$dns = 'mysql:host=localhost;dbname=jepsen-brite-db';
-$user = 'root';
-$pass = '';
+$dns = 'mysql:host=zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=k0m6n8erfpte57ew';
+$user = 'c5rad2qrrojzyd3c';
+$pass = 'd76kyt12ijglj4m0';
 $option = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 );
